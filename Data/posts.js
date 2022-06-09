1b2cd9466bb8141d0fdf60ceaad429324f74179d
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-06-09 10:33:01 -0400

    f post component ui , caption , comments , comment

--- Data/posts.js
+++ Data/posts.js
@@ -5,8 +5,8 @@ export const userposts = [
     {
         imageUrl : 'https://images.unsplash.com/photo-1610085927744-7217728267a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnVsbCUyMGhkJTIwd2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
         user: storiesData[0].userName,
-        likes : '7,402',
-        caption : 'A mountain is an elevated portion of the Earths crust generally with steep sides that show ',
+        likes : '7.402',
+        caption : 'A mountain is an elevated portion of the Earthscrust generally with steep sides that show ',
         profile_picture:  storiesData[0].image,
         Comments : [
             {
@@ -26,7 +26,7 @@ export const userposts = [
     {
         imageUrl : 'https://images.pexels.com/photos/719396/pexels-photo-719396.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
         user: storiesData[1].userName,
-        likes : '4,877',
+        likes : '4.877',
         caption : 'A mountain is an elevated portion of the Earths crust generally with steep sides that show ',
         profile_picture:  storiesData[1].image,
         Comments : [
--- components/posts/Fotterpost.jsx
+++ components/posts/Fotterpost.jsx
@@ -1,56 +1,87 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-//icons 
-import { FontAwesome } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
+//icons
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { color } from "react-native-reanimated";
 
-
-const Fotterpost = ({post}) => {
+const Fotterpost = ({ post }) => {
   return (
-      <> 
-      <View style={{ justifyContent:'space-between' , flexDirection:'row',alignItems:'center'}} > 
-    <View style={styles.IconContent} >
-     <FontAwesome name="heart-o" size={26} color="#fff" />
-     <FontAwesome name="comment-o" size={26} color="#fff" /> 
-     <MaterialCommunityIcons name="share-outline" size={26} color="#fff" />
-    </View> 
+    <>
+      <View
+        style={{
+          justifyContent: "space-between",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        {/* instgram icon  */}
+        <View style={styles.IconContent}>
+          <FontAwesome name="heart-o" size={26} color="#fff" />
+          <FontAwesome name="comment-o" size={26} color="#fff" />
+          <MaterialCommunityIcons name="share-outline" size={26} color="#fff" />
+        </View>
 
-    <View> 
-    <FontAwesome5 name="bookmark" size={24} color="#fff" />
-    </View>
-    </View> 
+        <View>
+          <FontAwesome5 name="bookmark" size={24} color="#fff" />
+        </View>
+      </View>
 
-    {/* likes */} 
-    <View style={{ marginTop:7,flexDirection:'row' }} >
-     <Text  style={{color:'#fff',fontWeight:'700'}} >{post.likes.toLocaleString()}  Likes</Text> 
-    </View> 
-
-    {/* caption */} 
-
-    <View style={{ marginTop:7 ,flexDirection:'row'  }}  > 
-      <Text>  
-         <Text style={{color:'#fff' ,fontWeight:'900' }} > {post.user} </Text> 
-         <Text style={{color:'#fff'  }} > {post.caption}</Text> 
+      {/* likes */}
+      <View style={{ marginTop: 5, flexDirection: "row" }}>
+        <Text style={{ color: "#fff", fontWeight: "500" }}>
+          {post.likes.toLocaleString()} Likes
         </Text>
-    </View>
+      </View>
+
+      {/* caption */}
+
+      <View style={{ marginTop: 5, flexDirection: "row" }}>
+        <Text>
+          <Text style={{ color: "#fff", fontWeight: "900" }}>
+          
+          {post.user}{" "}
+          </Text>
+          <Text style={{ color: "#fff" }}> {post.caption}</Text>
+        </Text>
+      </View>
+
+      {/* comment ternery */}
+      <View style={{ marginTop: 5 }}>
+        {!!post.Comments.length && (
+          <Text style={{ color: "gray" }}>
+            View {post.Comments.length > 1 ? "All " : ""} {post.Comments.length}
+            {post.Comments.length > 1 ? " Comments" : " Comment"}
+          </Text>
+        )}
+      </View> 
+
+      {/* Real comment  */} 
+
+     
+          
+       {post.Comments.map((comment,index) => (
+          <View key={index} style={{ marginTop: 5, flexDirection: "row" }}>
+          <Text style={{color:'white' }} > 
+          <Text style={{ fontWeight:'900' }}>{comment.user} </Text>
+          <Text>{comment.comment}</Text>
+          </Text>
+          
+          </View>
+       ))}  
+     
     </>
-  )
-}
+  );
+};
 
-export default Fotterpost  
-
-
-
-
-
-
+export default Fotterpost;
 
 const styles = StyleSheet.create({
-    IconContent : {
-        flexDirection:'row',
-        justifyContent:'space-between',
-        width:'35%'
-    }
-})
+  IconContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "35%",
+  },
+});
